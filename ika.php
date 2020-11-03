1c60c9bceb488a63b01350b9cb69ee3ee75f9928
AnnaG-M <73758735+AnnaG-M@users.noreply.github.com>
2020-11-03 20:01:33 +0200

    Create ika.php
    <!DOCTYPE html>
    <html>
      <head>
        <title>Ikätarkistus</title>
      </head>
      <body>
        <form action="ika.php" method="post">
          Kuinka vanha olet? <input type="text" name="ika"> <br>
          <input type="submit" value="Lähetä">
        </form>
      </body>
    </html>

---
+++ ika.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Ikätarkistus</title>
+  </head>
+  <body>
+    <form action="ika.php" method="post">
+      Kuinka vanha olet? <input type="text" name="ika"> <br>
+      <input type="submit" value="Lähetä">
+    </form>
+  </body>
+</html>
